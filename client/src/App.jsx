ffb4467b565562472069d45112fdc71abadd67da
HexSK <hex.ets2@gmail.com>
2026-03-30 14:23:21 +0200

    clientRoutes.js not needed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react';
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Hello world!</h1>
-       </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+        <Route path="/moj-ucet" element={<MojUcet />}></Route>
+        <Route path="/moj-ucet/prehlad" element={<MojUcetPrehlad />}></Route>
+        <Route path="/moj-ucet/zmluvy" element={<KlientZmluvy />}></Route>
+        <Route path="/moj-ucet/zmluva/:id_zmluva" element={<KlientZmluva/>}></Route>
+        <Route path="/moj-ucet/zmluva/:id_zmluva/faktury" element={<KlientZmluvaFaktury/>}></Route>
+        <Route path="/moj-ucet/zmluva/:id_zmluva/faktura/:id_faktura" element={<KlientZmluvaFaktura/>}></Route>
+        <Route path="/moj-ucet/zmluva/:id_zmluva/faktura/:id_faktura/zaplat" element={<ZaplatFakturu/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
